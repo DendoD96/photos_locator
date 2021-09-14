4fad7505da9a6db4e1a3847d22c619668231f6a3
Daniele <daniele.rossi18@studio.unibo.it>
2021-09-14 01:59:04 -0700

    Minor changes in README and setup.py files

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 <div style="text-align: left"> 
 <b>photos_locator</b> is a simple tool to rename your photos using datetime and gps metadata. Given a folder, it scrolls through all the photos and if they contain GPS metadata it renames them using the datetime and position in which they were taken. In detail:
 <ul>
-  <li>If the photo contains date and time of shooting and place, rename it in 'YYYY-MM-DDThh: mm: ss-location' format</li>
-  <li>If the photo contains only the date, rename it to 'YYYY-MM-DDThh: mm: ss-original name'</li>
+  <li>If the photo contains date and time of shooting and place, rename it in <b>YYYY-MM-DDThh:mm:ss-location</b></li>
+  <li>If the photo contains only the date, rename it to <b>YYYY-MM-DDThh:mm:ss-original name</b></li>
   <li>If the photo does not contain the date, do not rename it </li>
 </ul>  
 </div>
--- setup.py
+++ setup.py
@@ -3,19 +3,17 @@ from setuptools import setup, find_packages
 with open('README.md') as file:
 	readme_file = file.read()
 
-with open('LICENSE') as file:
-	license_file = file.read()
-
 setup(
 	name='photos_locator',
 	version='0.1.0',
 	description='A simple tool to rename your photos using gps metadata',
 	long_description=readme_file,
+	long_description_content_type='text/markdown',
 	author='Daniele Rossi',
 	author_email='daniele.rossi27@unibo.it',
 	url='https://github.com/DendoD96/photos_locator',
 	keywords=['photography', 'gps-location'],
-	license=license_file,
+	license='GPLv3',
 	packages=find_packages(exclude='tests'),
 	install_requires=[
 		'Pillow',
@@ -25,7 +23,7 @@ setup(
 	classifiers=[
 		'Development Status :: 3 - Alpha',
 		'Environment :: Console',
-		'Intended Audience :: Hobbyist photographer',
+		'Intended Audience :: End Users/Desktop',
 		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 		'Topic :: Utilities',
 		'Programming Language :: Python :: 3',
