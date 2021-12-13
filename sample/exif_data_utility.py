fed4fe20baa51696ce2dbede78f1cbb6e28772e7
Daniele <daniele.rossi18@studio.unibo.it>
2021-12-13 14:15:48 +0100

    Add overpass api call in order to find nearest point of interest

--- sample/exif_data_utility.py
+++ sample/exif_data_utility.py
@@ -6,84 +6,122 @@ import unidecode
 import functools
 from geopy.geocoders import Nominatim
 from typing import Dict
+import overpy
+from geopy import distance as compute_distance
+
+api = overpy.Overpass()
 
 DATETIME_DECIMAL_IFD_TAG = 36867
 DATETIME_DECIMAL_TIFF_TAG = 306
 
 GPS_DECIMAL_IFD_TAG = 34853
 
+OVERPASS_RADIUS = 250
+
+NOMINATIM_ZOOM = 14
+
 NOMINATIM_GEO_LOCATOR = Nominatim(user_agent='photoslocator')
 
 
 def _none_not_allowed(original_function):
-	@functools.wraps(original_function)
-	def new_function(*args, **kwargs):
-		if None in args:
-			raise ValueError("None is not a valid parameter")
-		return original_function(*args, **kwargs)
+    @functools.wraps(original_function)
+    def new_function(*args, **kwargs):
+        if None in args:
+            raise ValueError("None is not a valid parameter")
+        return original_function(*args, **kwargs)
 
-	return new_function
+    return new_function
 
 
 def _gps_degrees_minutes_seconds_to_decimal(d, m, s):
-	return d + float(m) / 60 + float(s) / 3600
+    return d + float(m) / 60 + float(s) / 3600
 
 
 def _get_longitude(gps_info: dict):
-	return _gps_degrees_minutes_seconds_to_decimal(gps_info[4][0], gps_info[4][1], gps_info[4][2])
+    return _gps_degrees_minutes_seconds_to_decimal(gps_info[4][0], gps_info[4][1], gps_info[4][2])
 
 
 def _get_latitude(gps_info: dict):
-	return _gps_degrees_minutes_seconds_to_decimal(gps_info[2][0], gps_info[2][1], gps_info[2][2])
+    return _gps_degrees_minutes_seconds_to_decimal(gps_info[2][0], gps_info[2][1], gps_info[2][2])
+
+
+@_none_not_allowed
+def _get_overpass_poi(latitude, longitude):
+    selected_tag_name = None
+    result = api.query(f"""
+            [out:json][timeout:25];
+            (
+                way["name"](around:{OVERPASS_RADIUS},{latitude},{longitude}); 
+            );
+            out center;
+            out tags;
+            """)
+    if len(result.ways) == 1:
+        selected_tag_name = result.ways[0].tags['name']
+
+    elif len(result.ways) >= 1:
+        last_distance = None
+        for way in result.ways:
+            distance = compute_distance.distance((latitude, longitude), (way.center_lat, way.center_lon)).km
+            if last_distance is None or distance < last_distance:
+                last_distance = distance
+                selected_tag_name = way.tags['name']
+
+    return selected_tag_name
 
 
 @_none_not_allowed
 def _get_datetime_iso8601(date_time_info: str):
-	date_time_split = date_time_info.split(' ')
-	return f"{date_time_split[0].replace(':', '-')}T{date_time_split[1]}"
+    date_time_split = date_time_info.split(' ')
+    return f"{date_time_split[0].replace(':', '-')}T{date_time_split[1].replace(':', '-')}"
 
 
 @_none_not_allowed
-def _get_location(gps_exif_information):
-	try:
-		latitude = _get_latitude(gps_exif_information)
-		longitude = _get_longitude(gps_exif_information)
-		location = re.sub('[^A-Za-z0-9,]+', '',
-		                  unidecode.unidecode(
-			                  NOMINATIM_GEO_LOCATOR.reverse(f"{latitude}, {longitude}", zoom=14).address)).split(',')
-		return f"{location[0]}_{location[1]}"
-	except Exception:
-		logging.error(f"Exception during coordinate parsing")
-		raise ValueError
+def _get_location(gps_exif_information: Dict):
+    try:
+        latitude = _get_latitude(gps_exif_information)
+        longitude = _get_longitude(gps_exif_information)
+        poi = _get_overpass_poi(latitude, longitude)
+        if poi is not None:
+            return poi
+        else:
+            location = re.sub('[^A-Za-z0-9,]+', '',
+                              unidecode.unidecode(
+                                  NOMINATIM_GEO_LOCATOR.reverse(f"{latitude}, {longitude}",
+                                                                zoom=NOMINATIM_ZOOM).address)).split(',')
+            return f"{location[0]} {location[1]}"
+    except Exception:
+        logging.error(f"Exception during coordinate parsing")
+        raise ValueError
 
 
 def get_location_and_datetime(photo_absolute_path: str) -> Dict[str, str]:
-	"""Returns a dictionary that can contain the location, date and time the photo was taken
+    """Returns a dictionary that can contain the location, date and time the photo was taken
 
-	:param: photo_absolute_path: The absolute path of the photo of which informations is needed
-	:type: photo_absolute_path: str
-	:return: a dictionary that can contain location and datetime (under location and datetime keys)
-	:rtype: Dict
-	:raises ValueError: the given file is not supported
-	"""
-	with Image.open(photo_absolute_path) as photo:
-		exif_data = photo._getexif()
-		location_datetime = {}
-		if exif_data:
-			gps_exif_information = exif_data.get(GPS_DECIMAL_IFD_TAG)
-			date_time_exif_information = exif_data.get(
-				DATETIME_DECIMAL_IFD_TAG) if DATETIME_DECIMAL_IFD_TAG in exif_data else exif_data.get(
-				DATETIME_DECIMAL_TIFF_TAG)
-			try:
-				location = _get_location(gps_exif_information)
-				location_datetime['location'] = location
-			except ValueError:
-				logging.warning("GPS exif data unavailable or not formatted correctly")
-				pass
-			try:
-				date_time = _get_datetime_iso8601(date_time_exif_information)
-				location_datetime['datetime'] = date_time
-			except ValueError:
-				logging.warning("Original date and time exif data unavailable")
-				pass
-		return location_datetime
+    :param: photo_absolute_path: The absolute path of the photo of which informations is needed
+    :type: photo_absolute_path: str
+    :return: a dictionary that can contain location and datetime (under location and datetime keys)
+    :rtype: Dict
+    :raises ValueError: the given file is not supported
+    """
+    with Image.open(photo_absolute_path) as photo:
+        exif_data = photo._getexif()
+        location_datetime = {}
+        if exif_data:
+            gps_exif_information = exif_data.get(GPS_DECIMAL_IFD_TAG)
+            date_time_exif_information = exif_data.get(
+                DATETIME_DECIMAL_IFD_TAG) if DATETIME_DECIMAL_IFD_TAG in exif_data else exif_data.get(
+                DATETIME_DECIMAL_TIFF_TAG)
+            try:
+                location = _get_location(gps_exif_information)
+                location_datetime['location'] = location
+            except ValueError:
+                logging.warning("GPS exif data unavailable or not formatted correctly")
+                pass
+            try:
+                date_time = _get_datetime_iso8601(date_time_exif_information)
+                location_datetime['datetime'] = date_time
+            except ValueError:
+                logging.warning("Original date and time exif data unavailable")
+                pass
+        return location_datetime
--- sample/photo_utils.py
+++ sample/photo_utils.py
@@ -5,50 +5,56 @@ import re
 
 from sample.exif_data_utility import get_location_and_datetime
 
-regex = r'^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([' \
-        r'0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?.*'
-match_iso8601 = re.compile(regex).match
+iso8601_without_colon_regex = r'^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][' \
+                              r'0-9])-([0-5][0-9])-([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?.*'
+match_iso8601_without_colon = re.compile(iso8601_without_colon_regex).match
 
 
 def _validate_iso8601(string_to_validate):
-	try:
-		if match_iso8601(string_to_validate) is not None:
-			return True
-	except re.error:
-		pass
-	return False
+    try:
+        if match_iso8601_without_colon(string_to_validate) is not None:
+            return True
+    except re.error:
+        pass
+    return False
 
 
 def rename_photos(photos_folder: str):
-	"""Rename all photos in a folder.
-	If the photo in question contains gps data and datetime it will be renamed to 'datetime-location'.
-	If the photo contains only datetime and has not already been renamed by the software
-	it will be renamed to 'datetime-original name'.
-	Otherwise it will not be renamed.
+    """Rename all photos in a folder.
+    If the photo in question contains gps data and datetime it will be renamed to 'datetime-location'.
+    If the photo contains only datetime and has not already been renamed by the software
+    it will be renamed to 'datetime-original name'.
+    Otherwise it will not be renamed.
 
-		:param: photos_folder: The folder that contains the photos
-		:type: photos_folder: str
-	"""
-	for photo_name in os.listdir(photos_folder):
-		photo_absolute_path = os.path.join(photos_folder, photo_name)
-		if os.path.isfile(photo_absolute_path):
-			try:
-				location_datetime = get_location_and_datetime(photo_absolute_path=photo_absolute_path)
-				if 'datetime' in location_datetime:
-					photo_filename_and_extension = os.path.splitext(photo_name)
-					photo_datetime = location_datetime['datetime']
-					try:
-						location = location_datetime['location']
-						os.rename(photo_absolute_path,
-						          f"{photos_folder}/{photo_datetime}-"
-						          f"{location}"
-						          f"{photo_filename_and_extension[1]}")
-					except KeyError:
-						original_file_name = photo_filename_and_extension[0]
-						if not _validate_iso8601(original_file_name):
-							os.rename(photo_absolute_path,
-							          f"{photos_folder}/{photo_datetime}-"
-							          f"{original_file_name}"
-							          f"{photo_filename_and_extension[1]}")
-			except PIL.UnidentifiedImageError:
-				logging.error(f"file extension of {photo_absolute_path} not supported!")
+    :param: photos_folder: The folder that contains the photos
+    :type: photos_folder: str
+    """
+
+    for photo_name in os.listdir(photos_folder):
+        photo_absolute_path = os.path.join(photos_folder, photo_name)
+        photo_filename_and_extension = os.path.splitext(photo_name)
+        original_file_name = photo_filename_and_extension[0]
+        file_extension = photo_filename_and_extension[1]
+
+        if os.path.isfile(photo_absolute_path) and not _validate_iso8601(original_file_name):
+            try:
+                location_datetime = get_location_and_datetime(photo_absolute_path=photo_absolute_path)
+                new_name = ""
+
+                try:
+                    photo_datetime = location_datetime['datetime']
+                    new_name += photo_datetime
+                except KeyError:
+                    logging.error(f"No datetime exif data for photo: {photo_absolute_path}. Leaving its original name")
+                    break
+
+                try:
+                    location = location_datetime['location']
+                    new_name += f"-{location}{file_extension}"
+                except KeyError:
+                    new_name += f"-{original_file_name}{file_extension}"
+
+                os.rename(photo_absolute_path, os.path.join(photos_folder, new_name))
+
+            except PIL.UnidentifiedImageError:
+                logging.error(f"file extension of {photo_absolute_path} not supported!")
